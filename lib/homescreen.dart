e1abdaaa271fc233f197635d66306353b7b1095c
ariouellette2000 <43147334+ariouellette2000@users.noreply.github.com>
2024-04-06 18:49:44 -0400

    HomeScreen done

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -3,6 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:log450_doit/ui/reusableWidgets/postItem.dart';
 import 'package:log450_doit/ui/utils/materialColor.dart';
+import 'package:log450_doit/ui/utils/sharedPreferences.dart';
 
 class HomeScreen extends StatefulWidget {
   static String routeName = '/home';
@@ -15,74 +16,97 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final TextEditingController _taskController = TextEditingController();
+  List<Map<String, String>> posts = [];
 
   @override
   void initState() {
     super.initState();
-    String currentUserId = '';
-    _getTasksOfFriends(currentUserId);
+    _getTasksOfFriends(SharedPreferences.shared.userId);
   }
 
   Future<void> _getTasksOfFriends(String userId) async {
-    final String friendsUrl = "http://10.0.2.2:3000/users/$userId/friends";
-    List<dynamic> friends = [];
+    final String userURL = "http://10.0.2.2:3000/users/$userId";
 
     try {
-      final responseFriends = await http.get(Uri.parse(friendsUrl), headers: {"Content-Type": "application/json"});
-      if (responseFriends.statusCode == 200) {
-        friends = jsonDecode(responseFriends.body);
+    final responseUser = await http.get(Uri.parse(userURL), headers: {"Content-Type": "application/json"});
+    if (responseUser.statusCode == 200) {
+      Map<String, dynamic> userData = jsonDecode(responseUser.body);
+      List<dynamic> friendIds = userData['friend_ids'];
 
-        for (var friend in friends) {
-          String friendId = friend['friend_user_id'];
-          print('Friend Id $friendId: $friendId');
+      for (var friendId in friendIds) {
+        final String friendsUrl = "http://10.0.2.2:3000/users/$friendId";
+        final responseFriends = await http.get(Uri.parse(friendsUrl), headers: {"Content-Type": "application/json"});
+
+        if (responseFriends.statusCode == 200) {
+          Map<String, dynamic> friendData = jsonDecode(responseFriends.body);
+          String friendName = "${friendData['username']}";
+
           final String tasksUrl = "http://10.0.2.2:3000/users/$friendId/tasks";
-
-          // Fetch tasks for each friend
           final responseTasks = await http.get(Uri.parse(tasksUrl), headers: {"Content-Type": "application/json"});
+
           if (responseTasks.statusCode == 200) {
             List<dynamic> tasks = jsonDecode(responseTasks.body);
-            print('Tasks for friend $friendId: $tasks');
+            
+            for (var task in tasks) {
+              String nameOfTask = task['title'];
+
+              Map<String, String> post = {
+                'image': 'assets/partyimage.jpg', //TODO: Change 'image' for real name of field
+                'nameOfPostUser': friendName,
+                'nameOfTask': task['title'],
+              };
+
+              setState(() {
+                posts.add(post);
+              });
+            }
           } else {
             print("Failed to get tasks for friend $friendId: ${responseTasks.body}");
           }
+        } else {
+          print("Failed to get friends: ${responseFriends.body}");
         }
-      } else {
-        print("Failed to get friends: ${responseFriends.body}");
       }
-    } catch (e) {
-      print("Error fetching data: $e");
+    } else {
+      print("Failed to get user data: ${responseUser.body}");
     }
+  } catch (e) {
+    print("Error fetching data: $e");
+  }
   }
 
   @override
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
+    if(posts.isEmpty){
+      return const Scaffold(
+        body: Center(
+          child: Text("Vous n'avez aucune actualité. \n Ajoutez plus d'amis!", 
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 18),
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
         body:SizedBox.expand(
                 child: Center(
-                    child:
-                        // TODO Populate with actual data from BD
-                        ListView(
-                            padding: const EdgeInsets.all(8),
-                            children: const <Widget>[
-                  PostItem(
-                      imagePath: "assets/partyimage.jpg",
-                      nameOfPostUser: "le boy SEGLAAAAA",
-                      nameOfTask: "cest quand mm nice Flutter fr"),
-                  PostItem(
-                      imagePath: "assets/amusementpark.jpg",
-                      nameOfPostUser: "BILAL",
-                      nameOfTask:
-                          "moi qui m'en sacre des corrections de mes eleves"),
-                  PostItem(
-                      imagePath: "assets/meeting.jpg",
-                      nameOfPostUser: "Un finissant de l'ETS",
-                      nameOfTask: "ma tronche a chaque matin"),
-                  PostItem(
-                      imagePath: "assets/paddleboard.jpg",
-                      nameOfPostUser: "la fomme du dep",
-                      nameOfTask: "fa bo fa chaud a 7h00 AM"),
-                ]))));
+                  child: ListView.builder(
+            padding: const EdgeInsets.all(8),
+            itemCount: posts.length,
+            itemBuilder: (BuildContext context, int index) {
+              var post = posts[index];
+              return PostItem(
+                imagePath: post['image']!,
+                nameOfPostUser: post['nameOfPostUser']!,
+                nameOfTask: post['nameOfTask']!,
+              );
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
 
